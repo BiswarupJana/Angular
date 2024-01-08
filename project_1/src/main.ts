cdedc9d91908b236fe17ad28c588ab7b15c96450
BiswarupJana <biswaruprx21@gmail.com>
2024-01-08 17:59:16 +0530

    #

--- project_1/src/main.ts
+++ project_1/src/main.ts
@@ -4,7 +4,7 @@ import {
 } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
-import routeConfig from './app/app.routes';
+import routeConfig from './app/routers';
 bootstrapApplication(AppComponent, {
   providers: [provideProtractorTestingSupport(), provideRouter(routeConfig)],
 }).catch((err) => console.error(err));
